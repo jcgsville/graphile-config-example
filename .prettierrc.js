a78a349a7c43b2ebb3429ef531ff9e13622c2b98
Benjie Gillam <benjie@jemjie.com>
2025-01-05 11:46:45 +0000

    Simplify: remove prettierrc; go with the defaults (but enable proseWrap)

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("prettier").Config} */
-export default {
-    bracketSameLine: true,
-    proseWrap: "always",
-    semi: false,
-    singleQuote: true,
-    tabWidth: 4,
-    trailingComma: 'all',
-}
